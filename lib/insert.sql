11c8c3bfac1376ae41e8c26b19b24b3851211cfa
KefahWambai <kefah.ngwei@student.moringaschool.com>
2023-02-23 10:18:37 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears ( 1, "Mr. Choclate", 5, "M", "black", "good", True);
+INSERT INTO bears ( 2, "Rowdy", 6, "M", "black", "good", True);
+INSERT INTO bears ( 3, "Sergent Brown", 2, "M", "black", "good", True);
+INSERT INTO bears ( 4, "Tabitha", 9, "F", "brown", "good", True);
+INSERT INTO bears ( 5, "Melissa", 3, "F", "white", "good", True);
+INSERT INTO bears ( 6, "Grinch", 5, "M", "black", "good", True);
+INSERT INTO bears ( 7, "Wendy", 2, "F", "black", "good", True);
+INSERT INTO bears (8, "", 9, "F", "Black", "good", True)
